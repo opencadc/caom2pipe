4fee445a5301a0461ff62bb34a3d9060edd56781
Sharon Goliath <Sharon.Goliath@nrc-cnrc.gc.ca>
2020-06-16 15:07:30 -0700

    s2650 - previews as png files for SPIRou spectrums.

--- caom2pipe/manage_composable.py
+++ caom2pipe/manage_composable.py
@@ -1255,8 +1255,8 @@ class PreviewVisitor(object):
             observation.observation_id))
         return {'artifacts': count}
 
-    def add_preview(self, uri, f_name, product_type):
-        self._previews[uri] = [f_name, product_type]
+    def add_preview(self, uri, f_name, product_type, mime_type='image/jpeg'):
+        self._previews[uri] = [f_name, product_type, mime_type]
 
     def add_to_delete(self, fqn):
         self._delete_list.append(fqn)
@@ -1308,7 +1308,7 @@ class PreviewVisitor(object):
             for entry in self._previews.values():
                 data_put(self._cadc_client, self._working_dir, entry[0],
                          self._archive, self._stream,
-                         mime_type=self._mime_type,
+                         mime_type=entry[2],
                          metrics=self._observable.metrics)
 
 
