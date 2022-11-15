e23f033561cbf0fc8d4b058cb74caf51cfcf48d3
Sharon Goliath <Sharon.Goliath@nrc-cnrc.gc.ca>
2022-11-15 10:54:33 -0800

    CADC-11992 - interim commit, LocalFilesDataSource integration tests are passing.

--- caom2pipe/tests/conftest.py
+++ caom2pipe/tests/conftest.py
@@ -16,12 +16,14 @@ def test_config():
     test_config.proxy_file_name = 'proxy.pem'
     test_config.proxy_fqn = os.path.join(tc.TEST_DATA_DIR, 'proxy.pem')
     test_config.work_file = 'todo.txt'
+    test_config.interval = 10  # minutes
     test_config.logging_level = 'INFO'
     test_config.log_file_directory = tc.TEST_DATA_DIR
     test_config.failure_fqn = f'{tc.TEST_DATA_DIR}/fail.txt'
     test_config.failure_log_file_name = 'fail.txt'
     test_config.retry_fqn = f'{tc.TEST_DATA_DIR}/retry.txt'
     test_config.retry_file_name = 'retry.txt'
+    test_config.state_file_name = 'state.yml'
     test_config.success_fqn = f'{tc.TEST_DATA_DIR}/good.txt'
     test_config.success_log_file_name = 'good.txt'
     test_config.rejected_fqn = f'{tc.TEST_DATA_DIR}/rejected.yml'
